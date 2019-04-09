88d46efbc28ddc67d6f07a3123ccf88df6688d67
Ross Dillon <rossdillon2@gmail.com>
2019-04-09 16:02:14 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('This is a new file');
+
+var x = 'a string of variable';
+console.log(x);
+
+console.log($);
+$( document ).ready(function() {
+    //Handler for .ready() called.
+    console.log('I am ready');
+});
